6c1595a3d9026f77110142317fe0e0aaa99024d0
nicklauszhanghoule <n.zhanghoule@gmail.com>
2020-06-11 23:43:16 -0400

    Updates

---
+++ precache-manifest.16a541105e747e60dd83cc0e3d2b67f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeb3ae2a6c09207d1b6f8dcbd57ee01",
+    "url": "/covid19-dashboard/index.html"
+  },
+  {
+    "revision": "3b2d3b406d233996df1d",
+    "url": "/covid19-dashboard/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "615939972dabba366f3f",
+    "url": "/covid19-dashboard/static/css/main.04887e23.chunk.css"
+  },
+  {
+    "revision": "3b2d3b406d233996df1d",
+    "url": "/covid19-dashboard/static/js/2.594ee849.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid19-dashboard/static/js/2.594ee849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615939972dabba366f3f",
+    "url": "/covid19-dashboard/static/js/main.e53d0f32.chunk.js"
+  },
+  {
+    "revision": "cc6ef1ed5de9ff9777e4",
+    "url": "/covid19-dashboard/static/js/runtime-main.b8c055d6.js"
+  }
+]);
